267a7fe48302b692cb8fd5ebc899d1581bf8aa62
James Robinson <james.robinson@applicable.com>
2025-10-01 14:03:26 +0100

    update

--- api-test.ps1
+++ api-test.ps1
@@ -1,11 +1,11 @@
-$uri = 'https://apim-uks-amp-core-dev-001.azure-api.net/insights/v1/voice/phonenumbers'
-Invoke-RestMethod -Uri $uri -Method Get
+$uri1 = 'https://apim-uks-amp-core-dev-001.azure-api.net/insights/v1/voice/phonenumbers'
+Invoke-RestMethod -Uri $uri1 -Method Get
  
-$uri = 'https://apim-uks-amp-core-dev-001.azure-api.net/insights/v1/voice/phonenumbers?sitecode=SFO1&status=assigned'
-Invoke-RestMethod -Uri $uri -Method Get
+$uri2 = 'https://apim-uks-amp-core-dev-001.azure-api.net/insights/v1/voice/phonenumbers?sitecode=SFO1&status=assigned'
+Invoke-RestMethod -Uri $uri2 -Method Get
 
 $uri3 = 'https://apim-uks-amp-core-dev-001.azure-api.net/insights/v1/voice/phonenumbers?first=1&sitecode=LON1&status=unassigned'
 Invoke-RestMethod -Uri $uri3 -Method Get
 
-$uri = 'https://apim-uks-amp-core-dev-001.azure-api.net/insights/v1/voice/phonenumbers/15550001007'
-Invoke-RestMethod -Uri $uri -Method Get
+$uri4 = 'https://apim-uks-amp-core-dev-001.azure-api.net/insights/v1/voice/phonenumbers/15550001007'
+Invoke-RestMethod -Uri $uri4 -Method Get
