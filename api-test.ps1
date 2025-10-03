3377ac9996b5e77dc22b584a1a94534a1f944be0
James Robinson <james.robinson@applicable.com>
2025-10-03 16:27:41 +0100

    update

--- api-test.ps1
+++ api-test.ps1
@@ -1,3 +1,5 @@
+#https://stuksampdevswagger01.z33.web.core.windows.net/temp/Demo.html
+
 $uri1 = 'https://apim-uks-amp-core-dev-001.azure-api.net/insights/v1/voice/phonenumbers'
 Invoke-RestMethod -Uri $uri1 -Method Get
  
@@ -13,6 +15,8 @@ Invoke-RestMethod -Uri $uri4 -Method Get
 $uri5 = 'https://apim-uks-amp-core-dev-001.azure-api.net/insights/v1/voice/phonenumbers?status=assigned'
 Invoke-RestMethod -Uri $uri5 -Method Get
 
+
+$uri6 = 'https://apim-uks-amp-core-dev-001.azure-api.net/insights/v1/voice/phonenumbers/+15550001003?status=assigned'
 $uri6 = 'https://apim-uks-amp-core-dev-001.azure-api.net/insights/v1/voice/phonenumbers/+15550001001?status=assigned'
 $uri6 = 'https://apim-uks-amp-core-dev-001.azure-api.net/insights/v1/voice/phonenumbers/+15550001005?status=assigned'
 Invoke-RestMethod -Uri $uri6 -Method Get
